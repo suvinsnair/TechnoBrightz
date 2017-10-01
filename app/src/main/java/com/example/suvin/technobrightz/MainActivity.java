8aacd36d53f0dc55b636b4a7b8ecafdbb12a0fc6
suvinsnair <suvin20156@gmail.com>
2017-10-02 01:03:26 +0530

    Project Finished..

--- app/src/main/java/com/example/suvin/technobrightz/MainActivity.java
+++ app/src/main/java/com/example/suvin/technobrightz/MainActivity.java
@@ -1,49 +1,116 @@
 package com.example.suvin.technobrightz;
 
-import android.content.res.ColorStateList;
-import android.graphics.Color;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private boolean doubleBackToExitPressedOnce;
+    private Handler mHandler = new Handler();
+
+    private final Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            doubleBackToExitPressedOnce = false;
+        }
+    };
+
     @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if (mHandler != null) { mHandler.removeCallbacks(mRunnable); }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press Again to Quit", Toast.LENGTH_SHORT).show();
+
+        mHandler.postDelayed(mRunnable, 2000);
+    }
+
+
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         WebView WebView = (WebView) findViewById(R.id.Web_View);
         WebSettings WebSettings = (WebSettings) WebView.getSettings();
         WebSettings.setJavaScriptEnabled(true);
+        WebView.setVerticalScrollBarEnabled(false);
+        WebSettings.setDefaultFontSize(10);
+        WebView.setHorizontalScrollBarEnabled(false);
+        Toast.makeText(this, "Hi ,Bitty", Toast.LENGTH_SHORT).show();
+
+
         final ProgressBar Progress_Bar = (ProgressBar) findViewById(R.id.Progress_Bar);
+        WebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+
+
+                return true;
+
+
+            }
+
+        });
 
 
 
 
         WebView.setWebChromeClient(new WebChromeClient() {
 
-            public void onProgressChanged(WebView view, int progress) {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                Progress_Bar.setProgress(newProgress);
 
-                if (progress == 100) {
-
+                if(newProgress==100){
 
                     Progress_Bar.setVisibility(View.INVISIBLE);
 
+
+            }else {
+
+                    Progress_Bar.setVisibility(View.VISIBLE);
                 }
-            }
+        }
         });
 
 
         String url = "http://www.alltopnow.com/";
 
         WebView.loadUrl(url);
+
+
+
+
+
+
+
+
     }
 }
+
